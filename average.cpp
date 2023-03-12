e357fba0a95b9024d7589b75cd9d7602b0859bbf
sippakon khammisawang <sippakon_k@cmu.ac.th>
2023-03-12 23:17:51 +0700

    full

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc,char* argv[]){
+    if(argc > 1){
+        cout<<"---------------------------------\n";
+        int sum=0;
+        for(int i=0;i<argc;i++){
+            sum=sum+atoi(argv[i]);
+        }
+        cout<<"Average of "<<argc-1<<" numbers = "<< sum/(argc-1);
+        cout<<"\n---------------------------------";
+    } else{
+        cout<<"Please input numbers to find average.";
+    }
+    
+    return 0;
+}
